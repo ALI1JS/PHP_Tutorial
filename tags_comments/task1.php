07a4731ad75b34e7f70e614b456fc7545deaac0c
ali ismail <aliismailh08@gmail.com>
2025-12-27 12:13:50 +0200

    update the task 1

--- tags_comments/task1.php
+++ tags_comments/task1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="<?='UTF-8'; ?>">
+    <meta charset="<?= 'UTF-8' ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= "My First PHP Page" ?></title>
@@ -14,7 +14,7 @@
          */
    
       ?>
-    <div><?php print"We Love" ?></div>
-    <div><?php print"We Love" ?></div>
+    <div><?php echo"We Love"; ?></div>
+    <div><?php echo"We Love"; ?></div>
   </body>
 </html>
